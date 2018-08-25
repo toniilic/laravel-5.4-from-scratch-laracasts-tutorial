c1b04ee374e0c85c99df07e4bf2b1c4d6209a904
toniilic <toni.ilic@net.hr>
2018-08-25 16:15:36 +0200

    Finished Eloquent 101

---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Task;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/tasks', function () {
+    $tasks = Task::all();
+
+    return view('tasks.index', compact('tasks'));
+});
+
+Route::get('/tasks/{task}', function ($id) {
+
+    $task = Task::find($id);
+
+
+    return view('tasks.show', compact('task'));
+});
+
+Route::get('about', function () {
+    return view('about');
+});
