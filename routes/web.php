2057f40ae2f5f3421f22ccab2ca14d5a098fba20
toniilic <toni.ilic@net.hr>
2018-08-26 12:45:49 +0200

    added Controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Task;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,20 +12,5 @@ use App\Task;
 |
 */
 
-Route::get('/tasks', function () {
-    $tasks = Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-
-    $task = Task::find($id);
-
-
-    return view('tasks.show', compact('task'));
-});
-
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
